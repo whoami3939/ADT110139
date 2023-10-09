9336c706c2efc043e8e7b806f2530a40b4c73f4e
whoami3939 <147360778+whoami3939@users.noreply.github.com>
2023-10-09 11:13:57 +0800

    Add files via upload

---
+++ 99乘法表.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+for i in range(1,10):
+    for j in range(1,10):
+        n=i*j
+        print("%d * %d =%2d"%(i,j,n))
+    
